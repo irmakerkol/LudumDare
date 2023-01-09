8383e5f22d26b96485507c01f858453784645420
baris-sinapli <43214847+baris-sinapli@users.noreply.github.com>
2023-01-09 11:21:52 +0300

    BugCatching mini game has been adapted into Canvas/UI

--- LudumDare52/Assets/0_Components/MiniGames/3_BugsGame/Scripts/ChangeCursor.cs
+++ LudumDare52/Assets/0_Components/MiniGames/3_BugsGame/Scripts/ChangeCursor.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class ChangeCursor : MonoBehaviour
 {
@@ -9,19 +10,26 @@ public class ChangeCursor : MonoBehaviour
     private Vector2 originalSize; // Original size of the cursor
     private AudioSource audioSource; // Reference to the AudioSource component
     public AudioClip shootSound; // Sound to play when the cursor is clicked
+    
+    private RectTransform uiElementRectTransform;
+    public Vector3 offset;
+    public RectTransform BasisObject;
+    public Camera cam;
 
     public bool isClickable = true; // Flag to keep track of whether the cursor is clickable
 
     private void Awake()
     {
+        Cursor.visible = false;
         audioSource = GetComponent<AudioSource>(); // Get a reference to the AudioSource component
+        uiElementRectTransform = GetComponent<RectTransform>(); // Get a reference to the RectTransform component
     }
     private void Start()
     {
-        Cursor.visible = false;
-        originalSize = transform.localScale;
+        originalSize = uiElementRectTransform.localScale; // Get the original size of the cursor
     }
-
+    /**
+     * Old Update
     void Update()
     {
         Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition); // Get the position of the cursor
@@ -38,6 +46,26 @@ public class ChangeCursor : MonoBehaviour
             isClickable = false;
         }
     }
+    */
+    void Update()
+    {
+        // Get the mouse position in screen space
+        Vector3 mousePos = Input.mousePosition + offset;
+        mousePos.z = BasisObject.position.z;
+        // Convert the mouse position to canvas space
+        uiElementRectTransform.position = cam.ScreenToWorldPoint(mousePos);
+
+        if (Input.GetMouseButtonDown(0) && isClickable)
+        {
+            // play the audioclip
+            audioSource.PlayOneShot(shootSound);
+            //sprRenderer.size += growAmount;
+            transform.localScale += (Vector3)growAmount;
+
+            StartCoroutine(ShrinkCursor());
+            isClickable = false;
+        }
+}
 
     // Coroutine to animate the scale of the cursor
     IEnumerator ShrinkCursor()
